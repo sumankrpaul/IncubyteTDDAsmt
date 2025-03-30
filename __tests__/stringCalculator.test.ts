c771303c7348fac6f8d3f25a51cab625234f5cfb
Suman Kumar Paul <suman_paul@avya-tech.com>
2025-03-31 02:43:14 +0530

    test to check function activity

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -5,3 +5,8 @@ it('myStringCalculator is callable function',()=>{
     expect(myStringCalculator).toBeInstanceOf(Function);
     expect(myStringCalculator.length).toBe(1);
 })
+
+it("Returns number and takes string as input ", ()=>{
+    const resp = myStringCalculator("");
+    expect(typeof resp).toBe("number");
+})
--- app/stringCalculator.ts
+++ app/stringCalculator.ts
@@ -1,1 +1,5 @@
-export function myStringCalculator(numbers: string){}
+export function myStringCalculator(numbers: string){
+    if(numbers === ""){
+        return 0;
+    }
+}
