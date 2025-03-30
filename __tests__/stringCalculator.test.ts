3114f5756304276fdc6bc1e85d65e682cc2ebdcf
Suman Kumar Paul <suman_paul@avya-tech.com>
2025-03-31 02:52:48 +0530

    test for 2 comma seperated number

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -16,5 +16,9 @@ it("Returns 0 on empty string and the number if only one number have been called
     const resp2 = myStringCalculator("2");
     expect(resp1).toBe(0);
     expect(resp2).toBe(2);
+})
 
+it("Returns sum of two comma seperated numbers", ()=>{
+    const sum = myStringCalculator("1,2");
+    expect(sum).toBe(3);
 })
--- app/stringCalculator.ts
+++ app/stringCalculator.ts
@@ -1,5 +1,8 @@
 export function myStringCalculator(numbers: string){
     if(numbers === ""){
         return 0;
+    } else if(numbers.split(',').length > 1){
+        const [num1, num2] = numbers.split(',').map(val=>parseInt(val));
+        return num1+num2;
     } else return +numbers;
 }
